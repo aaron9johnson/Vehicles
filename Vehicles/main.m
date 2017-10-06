fc499d7b837eff6f376a1f91fcf4134e9b6bc598
Aaron Johnson <aaron9johnson@gmail.com>
2017-10-06 16:40:50 -0700

    Initial Commit

---
+++ Vehicles/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Vehicles
+//
+//  Created by Aaron Johnson on 2017-10-06.
+//  Copyright © 2017 Aaron Johnson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
